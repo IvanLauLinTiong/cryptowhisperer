59e019cc6f250d6f040f85a08d7c1459823268b9
ivanllt <ivanlau91@hotmail.com>
2021-12-01 23:33:01 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from modzy import ApiClient
+from bs4 import BeautifulSoup
+from api_key import API_KEY
+import streamlit as st
+import requests
+import json
+import IPython
+
+st.title('MyApp')
+st.write(" # this is cool")
